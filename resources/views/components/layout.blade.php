824489b789c28984cfe9a3ec3c039488a7decbca
elbse <changpriego@gmail.com>
2025-08-25 21:25:08 +0800

    Working with tailwindcss

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ninja Network</title>
+
+    @vite('resources/css/app.css')
 </head>
 <body>
     <header>
         <nav>
-            <h1>Ninja Network</h1>
+            <h1 class="text-center px-8 py-10">Ninja Network</h1>
             <a href="/ninjas">All Ninjas</a>
             <a href="/ninjas/create">Create New Ninjas</a>
         </nav>
